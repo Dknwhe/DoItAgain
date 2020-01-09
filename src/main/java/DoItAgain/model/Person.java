7a2ebf45990045fb7ea0d252e7484f3eabc72746
Dknwhe <cai.mac@hotmail.com>
2020-01-09 13:48:34 +0100

    Person,Todo, test done!

---
+++ src/main/java/DoItAgain/model/Person.java
@@ -0,0 +1,34 @@
+package DoItAgain.model;
+
+public class Person {
+
+    private final int personId;
+    private String firstName;
+    private String lastName;
+
+    public Person (int personId, String firstName, String lastName) {
+        this.personId = personId;
+        setFirstName(firstName);
+        setLastName(lastName);
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/test/java/DoItAgain/model/PersonTest.java
@@ -0,0 +1,24 @@
+package DoItAgain.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PersonTest {
+
+
+    @Test
+    public void given_id_firstName_and_lastName_successfully_Create_object () {
+        String firstName = "Test";
+        String lastName = "Testsson";
+        int personId = 1;
+
+        Person testObject = new Person(personId,firstName,lastName);
+
+        assertEquals(personId,testObject.getPersonId());
+        assertEquals(firstName,testObject.getFirstName());
+        assertEquals(lastName,testObject.getLastName());
+
+    }
+
+}
